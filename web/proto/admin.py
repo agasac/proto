37243a66810951ce039808a7970d61b316da67f9
agasac <kento.shin@acceture.com>
2019-08-24 13:00:16 +0900

    .gitignore is now working

--- web/proto/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from proto.models import Prefecture, Spot
-
-admin.site.register(Prefecture)
-admin.site.register(Spot)
--- web/proto/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProtoConfig(AppConfig):
-    name = 'proto'
