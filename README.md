e08bfed51f525f229425c83f9b38e812c519867e
agasac <kento.shin@acceture.com>
2019-08-24 13:04:08 +0900

    add files

---
+++ README.md
@@ -0,0 +1,2 @@
+# proto
+A prototype web application.
---
+++ web/proto/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from proto.models import Prefecture, Spot
+
+admin.site.register(Prefecture)
+admin.site.register(Spot)
